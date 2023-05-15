380a29afa2b718948fd3b0283e74d635c16005bb
Bianca Oura <biancaoura@gmail.com>
2023-05-15 20:56:05 -0300

    feat: word search fn

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -3,11 +3,11 @@ from ting_file_management.file_management import txt_importer
 
 def exists_word(word, instance):
     file_path = instance.queue[0]
-
     text = txt_importer(file_path)
+
     if any(word.lower() in line.lower() for line in text):
         occurrences = [
-            {"linha": i+1}
+            {"linha": i + 1}
             for i, line in enumerate(text)
             if word.lower() in line.lower()
         ]
@@ -21,4 +21,21 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    file_path = instance.queue[0]
+    text = txt_importer(file_path)
+
+    if any(word.lower() in line.lower() for line in text):
+        occurrences = [
+            {
+                "linha": i + 1,
+                "conteudo": line
+            }
+            for i, line in enumerate(text)
+            if word.lower() in line.lower()
+        ]
+        return [{
+            "palavra": word,
+            "arquivo": file_path,
+            "ocorrencias": occurrences
+        }]
+    return []
