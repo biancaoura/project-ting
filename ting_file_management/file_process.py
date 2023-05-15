2e15931197d0f7b37dd18305a0abdfadb4db1fae
Bianca Oura <biancaoura@gmail.com>
2023-05-15 00:33:54 -0300

    feat: file process fn

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+
+
+def process(path_file: str, instance: Queue):
+    if path_file not in instance.queue:
+        file = txt_importer(path_file)
+        instance.enqueue(path_file)
+        dict_file = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(file),
+            "linhas_do_arquivo": file
+        }
+        print(dict_file, file=sys.stdout)
 
 
 def remove(instance):
