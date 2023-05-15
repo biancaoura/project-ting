2c43dd9a4da926e468733f7dcb46d91b723564e4
Bianca Oura <biancaoura@gmail.com>
2023-05-15 20:50:00 -0300

    feat: word existence/occurence check fn

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    file_path = instance.queue[0]
+
+    text = txt_importer(file_path)
+    if any(word.lower() in line.lower() for line in text):
+        occurrences = [
+            {"linha": i+1}
+            for i, line in enumerate(text)
+            if word.lower() in line.lower()
+        ]
+
+        return [{
+            "palavra": word,
+            "arquivo": file_path,
+            "ocorrencias": occurrences
+        }]
+    return []
 
 
 def search_by_word(word, instance):
